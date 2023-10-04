f654dc29b61c9c546b348365f60217b84583a2b8
Anvita67 <118798794+Anvita67@users.noreply.github.com>
2023-10-04 11:34:09 +0530

    Add files via upload

---
+++ Employee-management-project-STS/src/test/java/com/example/FirstWebApp/EmployeeManagementProjectStsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FirstWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementProjectStsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
